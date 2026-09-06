79b33c13a8a2f39cb9f9e24c75b0cb99f22c7194
avin <avinchen2006@gmail.com>
2026-09-06 22:14:34 +1200

    cleaned code and assigned M1, M2 with proper debounce. needs 4way hookup.

--- BasicMouse/BasicMouse.ino
+++ BasicMouse/BasicMouse.ino
@@ -2,8 +2,6 @@
 #include "PMW3360.h"
 //#include "BTfuncs.h"
 
-//TODO redesign mount for better optics, and do electronics
-
 // Pin definitions
 #define NCS_pin 7 // NCS
 #define MOT_pin 2 
@@ -14,21 +12,20 @@ bool buttonState[7] = {0};
 int8_t buttonMap[7] = {5,6,0,0,0,0,0};
 unsigned long lockTime[7] = {0};
 const unsigned long lockInterval = 10; //10ms
+unsigned long scrollTime = 0;
+unsigned long panTime = 0;
 const unsigned long scrollInterval = 200; //200ms
-
+unsigned long lastReportTime = 0;
 
 // PMW3360 sensor object
 PMW3360 sensor;
 //Params
 int CPI = 1600;
 
+const unsigned long reportInterval = 15; // this will depends on specific handshake, set this for minimum
 //BLE init
 BLEDis bledis; // device info 
 BLEHidAdafruit blehid; // HID functions
-
-unsigned long timesince = 0;
-
-
 
 //callbacks for bluetooth events
 void connect_callback(uint16_t conn_handle){
@@ -95,6 +92,7 @@ hid_mouse_report_t report{0,0,0,0,0};
 
 void loop() {
   if(Bluefruit.connected()){
+
     //switch state updater
     //change a switch state IF the input is different, and past the change timer
     //if not past the change timer, set the change timer to now
@@ -105,8 +103,8 @@ void loop() {
       }
     }
 
-    //update M1,2,3
-    for (int i = 0; i<4; i++){
+    //update M1,2,3 for report
+    for (int i = 0; i<2; i++){
       if(buttonState[i] == 0){
         report.buttons |= (1<<i);
       }
@@ -115,28 +113,52 @@ void loop() {
       }
     }
 
+    // //update report wheel state
+    // //just sums button presses in case rapid switch happens during one report
+    // if(millis() > scrollTime){
+    //   if(buttonState[5] == LOW){
+    //     report.wheel++;
+    //     scrollTime = millis()+scrollInterval;
+    //   }
+    //   if(buttonState[6] == LOW){
+    //     report.wheel--;
+    //     scrollTime = millis()+scrollInterval;
+    //   }
+    // }
 
+    // //update report pan state
+    // if(millis() > scrollTime){
+    //   if(buttonState[3] == LOW){
+    //     report.pan++;
+    //     scrollTime = millis()+scrollInterval;
+    //   }
+    //   if(buttonState[4] == LOW){
+    //     report.pan--;
+    //     scrollTime = millis()+scrollInterval;
+    //   }
+    // }
+
+    //get sensor burst data frame and report x and y - use an interrupt later on
+    //this may have to run a bit async as there may be several bursts per report.
     if(digitalRead(MOT_pin) == LOW){
       PMW3360_DATA data = sensor.readBurst(); 
       if (data.isMotion && data.isOnSurface) {
-        report.x = data.dx;
-        report.y = data.dy;
+        report.x += data.dx;
+        report.y += data.dy;
         //Serial.println(data.SQUAL);
       }
     }
-    else{
+
+    //UPDATE CONDITION LATER
+    if(millis()>lastReportTime+reportInterval){
+      blehid.mouseReport(&report);
+      lastReportTime = millis();
+      report.wheel = 0;
+      report.pan = 0;
       report.x = 0;
       report.y = 0;
     }
-    blehid.mouseReport(&report);
-      
+
+    
   }
-
-
-  // //generate BT HID report
-  // //Serial.println(digitalRead(M1));
-
-
-
-  
 }
